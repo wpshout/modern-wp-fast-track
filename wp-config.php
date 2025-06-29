b14908d5131ccc98eafa84f48d842de19bafdda4
Kaspars Dambis <hi@kaspars.net>
2025-06-29 18:04:53 +0300

    Add the homeurl too for consistency

--- wp-config.php
+++ wp-config.php
@@ -13,7 +13,10 @@ $table_prefix = 'wp_';
 // Keep the wp-contents outside of WP core directory.
 define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
 define( 'WP_CONTENT_URL', sprintf( 'http://%s/wp-content', $_SERVER['HTTP_HOST'] ) );
+
+// Keep wordpress outside of the web root.
 define( 'WP_SITEURL', sprintf( 'http://%s/wordpress', $_SERVER['HTTP_HOST'] ) );
+define( 'WP_HOME', sprintf( 'http://%s', $_SERVER['HTTP_HOST'] ) );
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {

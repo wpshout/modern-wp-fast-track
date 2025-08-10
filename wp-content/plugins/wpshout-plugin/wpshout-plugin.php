8e078df3c5e331f8897214c0fc05dbec882b17a5
Kaspars Dambis <hi@kaspars.net>
2025-08-10 19:11:31 +0300

    Add notice style to paragraph blocks

--- wp-content/plugins/wpshout-plugin/wpshout-plugin.php
+++ wp-content/plugins/wpshout-plugin/wpshout-plugin.php
@@ -2,3 +2,31 @@
 /**
  * Plugin Name: WPShout Plugin
  */
+
+add_action( 'init', 'wpshout_register_block_styles' );
+
+function wpshout_register_block_styles() {
+	register_block_style(
+		'core/paragraph',
+		[
+			'name' => 'notice',
+			'label' => __( 'Notice', 'wpshout-plugin' ),
+			// 'style_handle' => '...',
+			// 'inline_style' => 'p.is-style-notice { background:yellow; color:red; }',
+			'style_data' => [
+				'color' => [
+					'text' => 'var:preset|color|accent-3',
+					'background' => 'var:preset|color|accent-2',
+				],
+				'spacing' => [
+					'padding' => [
+						'top' => 'var:preset|spacing|30',
+						'bottom' => 'var:preset|spacing|30',
+						'left' => 'var:preset|spacing|30',
+						'right' => 'var:preset|spacing|30',
+					],
+				],
+			],
+		]
+	);
+}

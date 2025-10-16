5b979687e3536f253989cd589381ef1512e54498
Kaspars Dambis <hi@kaspars.net>
2025-10-16 16:03:02 +0300

    Pass keywords in PHP

--- wp-content/plugins/wpshout-plugin/wpshout-plugin.php
+++ wp-content/plugins/wpshout-plugin/wpshout-plugin.php
@@ -7,5 +7,16 @@ add_action( 'init', 'wpshout_register_blocks' );
 
 function wpshout_register_blocks() {
 	register_block_type( __DIR__ . '/build/blocks/faq' );
-	register_block_type( __DIR__ . '/build/blocks/faq-item' );
+
+	register_block_type( 
+		__DIR__ . '/build/blocks/faq-item',
+		[
+			// Pass keywords in PHP instead of block.json.
+			'keywords' => [
+				__( 'faq', 'wpshout' ),
+				__( 'question', 'wpshout' ),
+				__( 'answer', 'wpshout' ),
+			],
+		]
+	);
 }

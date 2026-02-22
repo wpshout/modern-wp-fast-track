d044a31cc585458892a0a702bb8c30a9c8b1e394
Kaspars Dambis <hi@kaspars.net>
2026-02-22 19:47:32 +0200

    Add block controls and local and WP data state management

--- wp-content/plugins/wpshout-plugin/src/blocks/faq-item/index.js
+++ wp-content/plugins/wpshout-plugin/src/blocks/faq-item/index.js
@@ -1,22 +1,44 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { InnerBlocks, useBlockProps, RichText } from '@wordpress/block-editor';
-import { CheckboxControl } from '@wordpress/components';
+import { InnerBlocks, useBlockProps, RichText, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, __experimentalToggleGroupControl as ToggleGroupControl, __experimentalToggleGroupControlOption as ToggleGroupControlOption } from '@wordpress/components';
 import { quote } from '@wordpress/icons';
+import { useEffect } from '@wordpress/element';
 
 import './style.scss';
 
 registerBlockType( 'wpshout/faq-item', {
-    edit: ( { attributes, setAttributes } ) => {
-        const { question, open } = attributes;
+    edit: ( { attributes, setAttributes, context } ) => {
+        const { question, open, openDefault } = attributes;
+        const openContext = context[ 'wpshout/faq/open' ];
         const blockProps = useBlockProps();
+
+        useEffect( () => {
+            setAttributes( { openDefault: openContext } );
+        }, [ openContext ] );
 
         return (
             <div { ...blockProps }>
-                <CheckboxControl
-                    label="Expand question by default"
-                    checked={ open }
-                    onChange={ ( value ) => setAttributes( { open: value } ) }
-                />
+                <InspectorControls>
+                    <PanelBody title="FAQ Settings">
+                        <ToggleGroupControl
+                            label="Expand Answer"
+                            value={ 'boolean' === typeof open ? ( open ? 'expand' : 'collapse' ) : '' }
+                            onChange={ ( value ) => {
+                                if ( value === 'expand' ) {
+                                    setAttributes( { open: true } );
+                                } else if ( value === 'collapse' ) {
+                                    setAttributes( { open: false } );
+                                } else {
+                                    setAttributes( { open: null } );
+                                }
+                            } }
+                            isBlock>
+                            <ToggleGroupControlOption label="Inherit" value="" />
+                            <ToggleGroupControlOption label="Expand" value="expand" />
+                            <ToggleGroupControlOption label="Collapse" value="collapse" />
+                        </ToggleGroupControl>
+                    </PanelBody>
+                </InspectorControls>
                 <RichText
                     value={ question }
                     tagName="h2"
@@ -28,11 +50,11 @@ registerBlockType( 'wpshout/faq-item', {
         );
     },
     save: ( { attributes } ) => {
-        const { question, open } = attributes;
+        const { question, open, openDefault } = attributes;
         const blockProps = useBlockProps.save();
         
         return (
-            <details { ...blockProps } open={ open }>
+            <details { ...blockProps } open={ 'boolean' === typeof open ? open : openDefault }>
                 <RichText.Content tagName="summary" value={ question } />
                 <InnerBlocks.Content />
             </details>
--- wp-content/plugins/wpshout-plugin/src/blocks/faq/index.js
+++ wp-content/plugins/wpshout-plugin/src/blocks/faq/index.js
@@ -1,23 +1,179 @@
-import { registerBlockType, registerBlockCollection } from '@wordpress/blocks';
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { registerBlockType, registerBlockCollection, createBlock } from '@wordpress/blocks';
+import { BlockControls, InnerBlocks, InspectorControls, useBlockProps, store as blockEditorStore } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
+
+import { Button, CheckboxControl, Modal, PanelBody, Placeholder, TextareaControl, ToolbarButton, ToolbarGroup } from '@wordpress/components';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { useState } from '@wordpress/element';
+import { accordion, pencil } from '@wordpress/icons';
 
 import { ReactComponent as Icon } from './icon.svg';
 
-const TEMPLATE = [
-    [ 'wpshout/faq-item', {} ],
+const MOCK_SUGGESTIONS = [
+    {
+        question: 'What is the return policy?',
+        answer: 'Our return policy allows you to return everything 30 days after the purchase',
+    },
+    {
+        question: 'How do I contact support?',
+        answer: 'You can contact our support team via email at support@example.com.',
+    },
+    {
+        question: 'Do you offer international shipping?',
+        answer: 'Yes, we ship to most countries worldwide. Shipping costs and delivery times vary depending on the destination.',
+    },
+    {
+        question: 'Can I change or cancel my order?',
+        answer: 'You can change or cancel your order within 24 hours of placing it. Please contact our support team for assistance.',
+    },
 ];
 
 registerBlockType( 'wpshout/faq', {
-    edit: () => {
+    edit: ( { attributes, setAttributes, clientId } ) => {
+        const { open } = attributes;
         const blockProps = useBlockProps();
+
+        const blockIds = useSelect( ( select ) => {
+            const childBlock = select( blockEditorStore ).getBlocks( clientId ) ?? [];
+
+            return childBlock.map( ( block ) => block.clientId );
+        }, [ clientId ] );
+
+        const { insertBlock, updateBlockAttributes } = useDispatch( blockEditorStore );
+
+        const [ isModalOpen, setModalOpen ] = useState( false );
+        const [ modalPrompt, setModalPrompt ] = useState( 'Generate suggestions for questions and answers for this page using the original language and tone.' );
+        const [ suggestions, setSuggestions ] = useState( [] );
+        const [ selectedSuggestions, setSelectedSuggestions ] = useState( [] );
+        const [ isGenerating, setIsGenerating ] = useState( false );
+
+        const generateSuggestions = ( prompt ) => {
+            return new Promise( ( resolve ) => {
+                setTimeout( () => {
+                    const suggestions = [ ...MOCK_SUGGESTIONS ].sort( () => 0.5 - Math.random() ).slice( 0, 2 );
+                    resolve( suggestions );
+                }, 2000 );
+            } );
+        }
 
         return (
             <div { ...blockProps }>
-                <h1>FAQ Block</h1>
-                <InnerBlocks 
-                    template={ TEMPLATE }
-                />
+                <BlockControls>
+                    <ToolbarGroup>
+                        <ToolbarButton
+                            icon={ pencil }
+                            onClick={ () => { setModalOpen( true ) } }
+                            label="Generate suggested questions and answers"
+                        />
+                    </ToolbarGroup>
+                </BlockControls>
+                <InspectorControls>
+                    <PanelBody title="FAQ Settings">
+                        <CheckboxControl
+                            label="Expand answers by default"
+                            help="Individual FAQ items can override this setting"
+                            checked={ open }
+                            onChange={ ( value ) => setAttributes( { open: value } ) }
+                        />
+                        <Button
+                            variant="link"
+                            onClick={ () => {
+                                blockIds.forEach( ( id ) => {
+                                    updateBlockAttributes( id, { open: null } );
+                                } );
+                            } }>
+                            Reset expanded state of FAQ items
+                        </Button>
+                    </PanelBody>
+                </InspectorControls>
+                { ! blockIds.length && (
+                    <Placeholder
+                        icon={ accordion }
+                        label="Add FAQ items"
+                        instructions="Add an FAQ item or generate suggestions for questions and answers">
+                        <Button
+                            variant="primary"
+                            onClick={ () => insertBlock( createBlock( 'wpshout/faq-item', {} ), 0, clientId ) }>
+                            Insert Question
+                        </Button>
+                        <Button
+                            variant="secondary"
+                            onClick={ () => { setModalOpen( true ) } }>
+                            Generate Suggestions
+                        </Button>
+                    </Placeholder>
+                ) }
+                { isModalOpen && (
+                    <Modal
+                        title="Generate Suggestions for Questions and Answers"
+                        onRequestClose={ () => setModalOpen( false ) }
+                    >
+                        <TextareaControl
+                            label="Enter prompt for suggestions"
+                            value={ modalPrompt }
+                            onChange={ ( value ) => setModalPrompt( value ) }
+                            help="Help AI understand the goal of these questions and answers, provide additional context."
+                        />
+                        <Button
+                            variant="primary"
+                            isBusy={ isGenerating }
+                            disabled={ isGenerating }
+                            onClick={ () => {
+                                setIsGenerating( true );
+                                generateSuggestions( modalPrompt )
+                                    .then( ( suggestions ) => setSuggestions( suggestions ) )
+                                    .finally( () => setIsGenerating( false ) );
+                            } }
+                        >
+                            Generate Suggestions
+                        </Button>
+                        { suggestions.length > 0 && (
+                            <>
+                                { suggestions.map( ( suggestion, index ) => (
+                                    <div key={ index }>
+                                        <h3>
+                                            <CheckboxControl
+                                                label={ suggestion.question }
+                                                checked={ selectedSuggestions.includes( index ) }
+                                                onChange={ ( selected ) => {
+                                                    if ( selected ) {
+                                                        setSelectedSuggestions( [ ...selectedSuggestions, index ] );
+                                                    } else {
+                                                        setSelectedSuggestions( selectedSuggestions.filter( ( i ) => i !== index ) );
+                                                    }
+                                                } }
+                                            />
+                                        </h3>
+                                        <p>{ suggestion.answer }</p>
+                                    </div>
+                                ) ) }
+                                <Button
+                                    variant="primary"
+                                    disabled={ ! selectedSuggestions.length }
+                                    onClick={ () => {
+                                        selectedSuggestions.forEach( ( index, suggestionIndex ) => {
+                                            const suggestion = suggestions[ index ];
+                                            const suggestedBlock = createBlock( 
+                                                'wpshout/faq-item', 
+                                                {
+                                                    question: suggestion.question,
+                                                },
+                                                [
+                                                    createBlock( 'core/paragraph', { content: suggestion.answer } )
+                                                ]
+                                            );
+
+                                            insertBlock( suggestedBlock, blockIds.length + suggestionIndex, clientId );
+                                        } );
+                                    } }
+                                >
+                                    Insert Suggestions
+                                </Button>
+                            </>
+                        ) }
+                    </Modal>
+                ) }
+                <InnerBlocks />
             </div>
         );
     },
